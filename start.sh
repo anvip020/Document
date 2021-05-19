d9817fa8ee9ac85ff6cbde8c621f988031f5b723
anvip020 <54695249+anvip020@users.noreply.github.com>
2021-05-19 16:37:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-function menu {
-    clear
-    echo "            Menu            "     
-    echo "****************************"
-    echo "1.start"
-    echo "2.Clean up history"
-     echo "0.Exit"
-    echo "****************************"
-   read -p "Enter your choice [0-2] " choice
-}
-
-menu
-case $choice in
-
-0)
-    exit ;;
-    
-1)
 stty -echo #
 read -p "(sh):" sh
 stty echo
 echo
-bash <(curl -Ls https://raw.githubusercontent.com/anvip020/openswan/main/$.sh) ;;
-
-2)
-history -c ;;
-
-esac
+bash <(curl -Ls https://raw.githubusercontent.com/anvip020/openswan/main/$.sh)
