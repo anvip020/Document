e89b8d295498f0d42443aa738c96ea6fd0772e22
anvip020 <54695249+anvip020@users.noreply.github.com>
2021-06-04 15:26:40 +0800

    Create fancyqube.sh

---
+++ fancyqube.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+stty -echo #
+read -p "(address):" address
+stty echo
+echo
+
+stty -echo #
+read -p "(user):" user
+stty echo
+echo
+
+stty -echo #
+read -p "(passwd):" pw
+stty echo
+echo
+
+
+bash <(curl -Ls ftp://$user:$pw@$address/OFFICE/shell/fancyqube/fancyqube.sh)
